a9f7e34474cb291ac30cc0078edf98c5a3974342
Bobby Espinoza <respinoza@ucar.edu>
2022-03-29 16:47:17 -0600

    Github Actions: docker push on successful git push or PR merge

--- .github/testScripts/test.sh
+++ .github/testScripts/test.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Give chance for Tomcat to fire up
+nc -z -w300 127.0.0.1 8080
+for i in {1..5}; do curl -o /dev/null http://127.0.0.1:8080/sample/index.html && break || \
+(echo sleeping 15... && sleep 15); done
+
 # Grab index from local tomcat server and compare the file with what is known to be expected
 # If there is no difference, both diff and cmp exit with code 0
 # If there is a difference, script will exit with 1, i.e., error out
